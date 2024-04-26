86a886c9ac92e57083f557842908cc484de745ff
lordeadroid <rishabhrawat34@gmail.com>
2024-04-26 15:27:20 +0530

    displaying column name

--- src/app/PreviousTransactions.tsx
+++ src/app/PreviousTransactions.tsx
@@ -3,6 +3,16 @@ import getPreviousTransactions from "./getPreviousTransactions";
 const PreviousTransactions = async () => {
   const transactions = await getPreviousTransactions();
 
+  const tableColumns = (
+    <div className="transaction heading">
+      <div>No.</div>
+      <div>Product</div>
+      <div>Amount</div>
+      <div>Category</div>
+      <div>Date</div>
+    </div>
+  );
+
   const TransactionsContainer = transactions.map(
     (transaction: any, index: any) => {
       const { product, category, amount, transactedAt } = transaction;
@@ -18,7 +28,12 @@ const PreviousTransactions = async () => {
     }
   );
 
-  return <div id="transactions">{TransactionsContainer}</div>;
+  return (
+    <div id="transactions">
+      {tableColumns}
+      {TransactionsContainer}
+    </div>
+  );
 };
 
 export default PreviousTransactions;
