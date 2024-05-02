03ba062835e5d22c177c483c29676e387877bf0a
lordeadroid <rishabhrawat34@gmail.com>
2024-05-02 18:14:32 +0530

    explicitly taking formdata as string

--- src/lib/set-cookies.ts
+++ src/lib/set-cookies.ts
@@ -5,10 +5,12 @@ const setCookies = async (formData: FormData) => {
   const username = formData.get("username");
   const password = formData.get("password");
 
-  const hash = createHmac("sha512", password).digest("hex");
-  const sessionKey = createHmac("sha512", hash).update(username).digest("hex");
+  const hash = createHmac("sha512", password as string).digest("hex");
+  const sessionKey = createHmac("sha512", hash)
+    .update(username as string)
+    .digest("hex");
   const expires = new Date(Date.now() + 60 * 60 * 1000);
-  cookies().set("session", sessionKey, { expires });
+  cookies().set("session", sessionKey, { expires, httpOnly: true });
 };
 
 export default setCookies;
