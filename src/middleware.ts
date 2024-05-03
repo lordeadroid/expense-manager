d7f7375e154db5cda2873b1965c68f25da95a0d2
lordeadroid <rishabhrawat34@gmail.com>
2024-05-03 14:45:37 +0530

    handling signup route separately

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,12 @@ import { NextRequest, NextResponse } from "next/server";
 export default async function middleware(request: NextRequest) {
   const cookie = request.cookies.get("session");
 
+  const url = new URL(request.url);
+
+  if (url.pathname === "/signup") {
+    return NextResponse.next();
+  }
+
   if (cookie?.value) {
     return NextResponse.next();
   }
