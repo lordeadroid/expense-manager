5466855e3c0b2d69e34b4e677b0c8aaf182898d5
lordeadroid <rishabhrawat34@gmail.com>
2024-05-06 11:26:09 +0530

    verification check before setting cookie

--- src/lib/set-cookies.ts
+++ src/lib/set-cookies.ts
@@ -7,14 +7,19 @@ const setCookies = async (formData: FormData) => {
   const username = formData.get("username");
   const password = formData.get("password");
 
-  const isVerified: boolean = await isVerifiedUser(username, password);
+  const hash = createHmac("sha512", password as string).digest("hex");
+  const hashedPassword = createHmac("sha512", hash)
+    .update(username as string)
+    .digest("hex");
+
+  const isVerified: boolean = await isVerifiedUser(
+    username as string,
+    hashedPassword
+  );
+
   if (isVerified) {
-    const hash = createHmac("sha512", password as string).digest("hex");
-    const sessionKey = createHmac("sha512", hash)
-      .update(username as string)
-      .digest("hex");
     const expires = new Date(Date.now() + 60 * 60 * 1000);
-    cookies().set("session", sessionKey, { expires, httpOnly: true });
+    cookies().set("session", hashedPassword, { expires, httpOnly: true });
   }
 };
 
