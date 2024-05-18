43ed516489861171d42cb04afde22b8a035edbdb
lordeadroid <rishabhrawat34@gmail.com>
2024-05-18 17:15:50 +0530

    modified setting cookies function

--- src/lib/set-cookies.ts
+++ src/lib/set-cookies.ts
@@ -1,26 +1,10 @@
-import { createHmac } from "crypto";
 import { cookies } from "next/headers";
 
-import isVerifiedUser from "./is-verified-user";
+const setCookies = async (username: string, hashedPassword: string) => {
+  const expires = new Date(Date.now() + 60 * 60 * 1000);
 
-const setCookies = async (formData: FormData) => {
-  const username = formData.get("username");
-  const password = formData.get("password");
-
-  const hash = createHmac("sha512", password as string).digest("hex");
-  const hashedPassword = createHmac("sha512", hash)
-    .update(username as string)
-    .digest("hex");
-
-  const isVerified: boolean = await isVerifiedUser(
-    username as string,
-    hashedPassword,
-  );
-
-  if (isVerified) {
-    const expires = new Date(Date.now() + 60 * 60 * 1000);
-    cookies().set("session", hashedPassword, { expires, httpOnly: true });
-  }
+  cookies().set("username", username, { expires, httpOnly: true });
+  cookies().set("session", hashedPassword, { expires, httpOnly: true });
 };
 
 export default setCookies;
